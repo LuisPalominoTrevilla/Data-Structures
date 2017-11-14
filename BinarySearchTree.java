19ce8bc5da4c43c80a69590a6fe7432160bd076e
LuisPalominoTrevilla <luispalominot3@gmail.com>
2017-11-13 20:50:50 -0600

    Almost finished BST

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class BinarySearchTree <K extends Comparable<K>, V>{
 
@@ -47,6 +50,39 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         }
     }
     
+    public void put(K key, V value){
+        if(key == null || value == null){
+            throw new NullPointerException();
+        }
+        if(this.isEmpty()){
+            this.root = new TreeNode<K, V>(key, value);
+            return;
+        }
+        int cmp;
+        TreeNode<K, V> current = this.root;
+        TreeNode<K, V> prev = null;
+        while(current != null){
+            prev = current;
+            cmp = key.compareTo(current.key);
+            if(cmp > 0){
+                current = current.rightChild;
+            }else if(cmp < 0){
+                current = current.leftChild;
+            }else{
+                current.value = value;
+                return;
+            }
+        }
+        cmp = key.compareTo(prev.key);
+        if(cmp > 0){
+            prev.rightChild = new TreeNode<K, V>(key, value);
+            prev.rightChild.parent = prev;
+        }else{
+            prev.leftChild = new TreeNode<K, V>(key, value);
+            prev.leftChild.parent = prev;
+        }
+    }
+
     public V remove(K key){
         TreeNode<K, V> node = this.searchNode(key, this.root);
         if(node != null){
@@ -69,6 +105,7 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
             }else{
                 remove.parent.rightChild = null;
             }
+            
         }else if(remove.leftChild == null && remove.rightChild != null || remove.leftChild != null && remove.rightChild == null){
             // If the remove node only has one child
             if(remove.leftChild != null){
@@ -105,10 +142,14 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         return this.max(node.leftChild);
     }
     
-    private TreeNode<K, V> min(TreeNode<K, V> node){
+    private TreeNode<K, V> getSuccesor(TreeNode<K, V> node){
         if(node == null){
             return null;
         }
+        return this.min(node.rightChild);
+    }
+    
+    private TreeNode<K, V> min(TreeNode<K, V> node){
         if(node.leftChild == null){
             return node;
         }
@@ -122,7 +163,7 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         return this.max(node.rightChild);
     }
     
-    public V search(K key){
+    public V get(K key){
         if(key == null){
             throw new NullPointerException();
         }
@@ -131,6 +172,10 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         }else{
             return (this.searchNode(key, this.root) == null)? null:this.searchNode(key, this.root).value;
         }
+    }
+    
+    public boolean contains(K key){
+        return (this.get(key) == null)? false:true;
     }
     
     private TreeNode<K, V> searchNode(K key, TreeNode<K, V> node){
@@ -163,6 +208,21 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         return acum;
     }
     
+    public String descendente(){
+        return this.descendente(this.root);
+    }
+    
+    private String descendente(TreeNode<K, V> node){
+        String acum ="";
+        if(node == null){
+            return "";
+        }
+        acum += this.descendente(node.rightChild);
+        acum += node;
+        acum += this.descendente(node.leftChild);
+        return acum;
+    }
+    
     public String preorder(){
         return this.preorder(this.root);
     }
@@ -175,6 +235,31 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         acum += node;
         acum += this.preorder(node.leftChild);
         acum += this.preorder(node.rightChild);
+        return acum;
+    }
+    
+    public String preOrderIt(){
+        String acum = "";
+        Stack<TreeNode<K, V>> stack = new Stack<>();
+        stack.push(this.root);
+        TreeNode<K, V> current = this.root;
+        while(!stack.isEmpty()){
+            acum += current;
+            if(current.leftChild != null){
+                current = current.leftChild;
+                stack.push(current);
+            }else{
+                TreeNode<K, V> newNode = stack.pop();
+                while(newNode.rightChild == null){
+                    if(stack.isEmpty()){
+                        return acum;
+                    }
+                    newNode = stack.pop();
+                }
+                current = newNode.rightChild;
+                stack.push(current);
+            }
+        }
         return acum;
     }
     
@@ -191,6 +276,138 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         acum += this.postorder(node.rightChild);
         acum += node;
         return acum;
+    }
+    
+    public String porNiveles(){
+        String res = "";
+        Queue<TreeNode<K, V>> visitNodes = new LinkedList<>();
+        visitNodes.add(this.root);
+        TreeNode<K, V> currentNode;
+        while(!visitNodes.isEmpty()){
+            currentNode = visitNodes.remove();
+            res += currentNode;
+            if(currentNode.leftChild != null){
+                visitNodes.add(currentNode.leftChild);
+            }
+            if(currentNode.rightChild != null){
+                visitNodes.add(currentNode.rightChild);
+            }
+        }
+        return res;
+    }
+    
+    public String mayoresA(K key){
+        TreeNode<K, V> node = this.searchNode(key, this.root);
+        if(node == null){
+            return null;
+        }else{
+            return this.mayoresA(this.root, key);
+        }
+    }
+    
+    private String mayoresA(TreeNode<K, V> node, K key){
+        String acum ="";
+        if(node == null){
+            return "";
+        }
+        acum += this.mayoresA(node.leftChild, key);
+        acum += (node.key.compareTo(key) > 0)? node:"";
+        acum += this.mayoresA(node.rightChild, key);
+        return acum;
+    }
+    
+    public String cualesPorNivel(int n){
+        Queue<TreeNode<K, V>> levelNodes = new LinkedList<>();
+        levelNodes.add(this.root);
+        return this.cualesPorNivel(levelNodes, n, levelNodes.size());
+    }
+    
+    private String cualesPorNivel(Queue<TreeNode<K, V>> levelNodes, int n, int numElements){
+        // If we have arrived to the desired level
+        if(numElements == 0){
+            return "";
+        }
+        if(n == 0){
+            String acum = "";
+            for(TreeNode<K, V> node:levelNodes){
+                acum += node;
+            }
+            return acum;
+        }
+        else{
+            for(int i = 0; i < numElements; i++){
+                TreeNode<K, V> current = levelNodes.remove();
+                if(current.leftChild != null) levelNodes.add(current.leftChild);
+                if(current.rightChild != null) levelNodes.add(current.rightChild);
+                
+            }
+            return this.cualesPorNivel(levelNodes, --n, levelNodes.size());
+        }
+        
+        
+    }
+    
+    public int size(){
+        return this.size(this.root);
+    }
+    
+    private int size(TreeNode<K, V> node){
+        int acum = 0;
+        if(node == null){
+            return 0;
+        }
+        acum += this.size(node.leftChild);
+        acum += 1;
+        acum += this.size(node.rightChild);
+        return acum;
+    }
+    
+    public int cuantasHojas(){
+        return this.cuantasHojas(this.root);
+    }
+    
+    private int cuantasHojas(TreeNode<K, V> node){
+        int numHojas = 0;
+        if(node == null){
+            return 0;
+        }
+        numHojas += this.cuantasHojas(node.leftChild);
+        numHojas += (node.leftChild == null && node.rightChild == null)? 1:0;
+        numHojas += this.cuantasHojas(node.rightChild);
+        return numHojas;
+    }
+    
+    public String hojas(){
+        return this.hojas(this.root);
+    }
+    
+    private String hojas(TreeNode<K, V> node){
+        String acum ="";
+        if(node == null){
+            return "";
+        }
+        acum += this.hojas(node.leftChild);
+        acum += (node.leftChild == null && node.rightChild == null)? node:"";
+        acum += this.hojas(node.rightChild);
+        return acum;
+    }
+    
+    public int altura(){
+        if(this.isEmpty()){
+            return 0;
+        }
+        int h1 = this.altura(this.root.leftChild);
+        int h2 = this.altura(this.root.rightChild);
+        return Math.max(h1, h2);
+    }
+    
+    public int altura(TreeNode<K, V> node){
+        if(node == null){
+            return 0;
+        }
+        int altura1 = this.altura(node.leftChild);
+        int altura2 = this.altura(node.rightChild);
+        return Math.max(altura1, altura2) + 1;
     }
     
     static class TreeNode<K extends Comparable<K>, V>{
@@ -211,14 +428,16 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
     
     public static void main(String[] args){
         BinarySearchTree<Integer, Integer> bs = new BinarySearchTree<>();
+        bs.put(7, 7);
+        bs.put(2, 2);
+        bs.put(5, 5);
+        bs.put(-3, -3);
+        bs.put(9, 9);
+        bs.put(1, 1);
+        bs.put(3, 3);
         bs.add(4, 4);
-        bs.add(2, 2);
-        bs.add(3, 3);
-        bs.add(7, 7);
-        bs.add(6, 6);
-        bs.add(9, 9);
-        bs.add(5, 5);
-        bs.remove(2);
-        System.out.println(bs.inorder());
+        bs.put(8, 8);
+        bs.put(10, 10);
+        System.out.println(bs.cuantasHojas());
     }
 }
